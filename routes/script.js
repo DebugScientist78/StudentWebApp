5b6a3eb4456ea12ad097150978bf1c16c16c3589
Richie Motorgeanu <ric.motorgeanu@gmail.com>
2021-04-24 13:00:43 -0400

    Caluclator works in console

--- routes/script.js
+++ routes/script.js
@@ -1,9 +1,58 @@
 function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
-    console.log("Hi");
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
   }
+  console.log("Hi");
+}
+
+function addMoreRow() {
+  var table = document.getElementById("calculatorTable");
+
+  var totalRows= table.rows.length;
+
+  var row = table.insertRow(totalRows-1);
+
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+
+  cell1.innerHTML = '<input type="text" name="Weight" size="30">';
+  cell2.innerHTML = '<input type="text" name="Weight" size="30">';
+}
+
+function calculate() {
+  var table = document.getElementById("calculatorTable");
+  var totalRows = table.rows.length;
+
+  var totalWeight = 0;
+  var missingWeight = 0;
+  var totalMark = 0;
+  var bestCase = 0;
+  var worstCase = 0;
+  var currentAverage = 0;
+  var multiplierFactor = 0;
+
+  for(var i = 1; i < totalRows-1; i++) {
+    totalWeight += parseInt(table.rows[i].cells[1].firstChild.value);
+  }
+
+  missingWeight = 100 - totalWeight;
+  multiplierFactor = 100 / totalWeight;
+
+  for(var i = 1; i < totalRows-1; i++) {
+    totalMark += parseInt(table.rows[i].cells[0].firstChild.value) * parseInt(table.rows[i].cells[1].firstChild.value) * 0.01;
+  }
+
+  for(var i = 1; i < totalRows-1; i++) {
+    currentAverage += parseInt(table.rows[i].cells[0].firstChild.value) * parseInt(table.rows[i].cells[1].firstChild.value) * multiplierFactor * 0.01;
+  }
+
+  worstCase = totalMark;
+  bestCase = totalMark + missingWeight;
+
+  console.log(worstCase);
+  console.log(bestCase);
+  console.log(currentAverage);
+}
