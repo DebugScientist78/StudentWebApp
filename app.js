cf8be9149997239a6da70f226701bf5b2d5bd4a7
Chency <chency.wang@gmail.com>
2021-04-25 13:51:54 -0400

    Fixed Linking different webpages
    Home can now link to Calendar and Calculator and back to home

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/schedule', usersRouter);
+app.use('/calculator', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
-const { Console } = require('console');
 const express = require('express');
-const mysql = require('mysql2');
 const router = express.Router();
-const app = express();
 var path = require('path');
 
-app.get('/calculator', function(req, res) {
+router.get('/calculator', function(req, res) {
     console.log(path);
     res.sendFile(path.join(__dirname + '/calculator.html'));
 });
@@ -16,7 +13,5 @@ router.get('/', function(req, res, next) {
     title: 'Student Web App'
   });
 });
-
-app.listen(8000);
 
 module.exports = router;
