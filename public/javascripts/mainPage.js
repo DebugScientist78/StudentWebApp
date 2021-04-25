3e9f5781871dba27916bff3aae1cd500950e9c11
Chency <chency.wang@gmail.com>
2021-04-25 01:47:32 -0400

    Page Switching, Calendar Visual added
    Fixed main page for mobile.
    Used FullCalendar.io API to display calendar
    Added Navigation Bar to return to home page.

--- public/javascripts/mainPage.js
+++ public/javascripts/mainPage.js
@@ -1,3 +1,18 @@
-function logButton(val) {
-    console.log(val);
-}
+
+
+jQuery(function() {
+    $("#buttonSchedular").on("click", function() {
+        console.log("schedular");
+    });
+
+    var localPath = window.location.pathname;
+    if (localPath == "/schedule") {
+        //generate calender
+        console.log("MAKE CALENDER");
+        var calendarID = document.getElementById("calendar");
+        var calendar = new FullCalendar.Calendar(calendarID,{
+            initialView: 'dayGridMonth'
+        });
+        calendar.render();
+    }
+});
